411b66ee509996e4cf0de1a12d98cd870e2d9cc4
Joyselena18 <joytesapebyolap18@gmail.com>
2025-06-07 17:17:06 +0700

    chore: save local changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-
 import "@/styles/tailwind.css";
 import { Metadata } from "next";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // pastikan import css-nya
 import WithNavbar from "@/components/Layout/WithNavbar";
 import WithFooter from "@/components/Layout/WithFooter";
 
 export const metadata: Metadata = {
   title: "EPW 16",
   description: "EPW 16 WEBSITES",
-  keywords: "EPW",
+  keywords: ["EPW"],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/components/Logo.svg" />
+      <head>
+        <link rel="icon" href="/components/Logo.svg" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>
-        <WithNavbar/>
+        <WithNavbar />
         <main>{children}</main>
         <ToastContainer />
         <WithFooter />
